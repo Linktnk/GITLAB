c3fa618bc411f302080c6d011ae791473cc323e7
Linktnk <link_iam@hotmail.com>
2017-04-24 12:24:27 +0700

    Change bloodgroup.c

--- bloodgroup.c
+++ bloodgroup.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 void main()
-{ char Name[50], G[5], answer[10];
+{ char Name[100], G[5], answer[10];
   int i;
   do
   {
-    printf("What is your name? ");
+    printf("What is your Full Name? ");
     scanf("%s", Name);
     do
     {
